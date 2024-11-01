6297d2a03e646a584303fc562ed6bd3e630dc867
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-01 13:55:04 +0800

    Revised twosum code

--- twosum/TwoSum.java
+++ twosum/TwoSum.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i=0; i<nums.length;i++){
-            int searchingFor = target - nums[i];
-            for (int j=1; j<nums.length;j++){
+        for (int i=0; i<nums.length; i++){
+            int toFind = target - nums[i];
+            for (int j=0; j<nums.length;j++){
                 if (i==j){
                     continue;
                 }
-                if (nums[j]==searchingFor){
-                    int[] solution = {i,j};
-                    return solution;
+                if (nums[j]==toFind){
+                    int[] toReturn = {i,j};
+                    return toReturn;
                 }
             }
+            
         }
-    int[] nothing = new int[2];
-    return nothing;
+        return null;
     }
 }
