810079efbb65fedeae75237ec15a35c5556219e0
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-04 15:18:32 +0800

    Updated the code using Arrays.sort method
    To try the suggested methods online after

--- MergeSortedArray/Solution.java
+++ MergeSortedArray/Solution.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 class Solution {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
@@ -8,21 +9,6 @@ class Solution {
             nums1[x]=nums2[z];
             z++;
         }
-        int placeHolder = 0;
-        while (true){
-            int swapCount = 0;
-            //swap internally by comparing current value and its next
-            for (int i=1; i<length; i++){
-                if (nums1[i-1]>nums1[i]){
-                    placeHolder =nums1[i-1];
-                    nums1[i-1]=nums1[i];
-                    nums1[i]= placeHolder;
-                    swapCount++;
-                }
-            }
-            if (swapCount==0){
-                break;
-            }
-        }
+        Arrays.sort(nums1);;        
     }
 }
