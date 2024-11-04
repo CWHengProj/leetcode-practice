a759ceb3005c0f34a3023d21ab65ea5a31e140a1
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-04 12:33:45 +0800

    passed the test cases

--- MergeSortedArray/Solution.java
+++ MergeSortedArray/Solution.java
@@ -1,31 +1,27 @@
 class Solution {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //trying insertion sort
-        //run through nums1, compare each int with nums2. if nums2 smaller, swap.
-        //ignore 0. if there is no swaps happening, append all to the list.
-        int placeHolder =0;
+        //moves all the variables to nums1
+        int length = m+n;
+        int z=0;
+        for (int x=m; x<length; x++){
+            nums1[x]=nums2[z];
+            z++;
+        }
+        int placeHolder = 0;
         while (true){
-            int swapCount =0;
-            for (int i=0; i<nums1.length;i++){
-                for(int j=0; j<nums2.length; j++){
-                    if (nums1[i]>nums2[j]){
-                        placeHolder=nums1[i];
-                        nums1[i]=nums2[j];
-                        nums2[j]=placeHolder;
-                        swapCount++;
-                    }
+            int swapCount = 0;
+            //swap internally by comparing current value and its next
+            for (int i=1; i<length; i++){
+                if (nums1[i-1]>nums1[i]){
+                    placeHolder =nums1[i-1];
+                    nums1[i-1]=nums1[i];
+                    nums1[i]= placeHolder;
+                    swapCount++;
                 }
             }
             if (swapCount==0){
                 break;
-            }
-        }
-        int q=0;
-        for (int l=0;l<nums1.length;l++){
-            if (nums1[l]==0){
-                nums1[l]=nums2[q];
-                q++;
             }
         }
     }
