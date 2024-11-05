7472932a8f2ce2f383b991be389bd7448ea07bea
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-05 10:11:42 +0800

    Added top solution submitted by other users
    Top solution decrements from the back, and adds the current value depending on whether nums1 or nums2 is larger, to the back of the line. Can be done as the arrays are already sorted.

--- MergeSortedArray/Solution.java
+++ MergeSortedArray/Solution.java
@@ -1,14 +1,15 @@
-import java.util.Arrays;
-class Solution {
-
+class Solution {//user top solution
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //moves all the variables to nums1
-        int length = m+n;
-        int z=0;
-        for (int x=m; x<length; x++){
-            nums1[x]=nums2[z];
-            z++;
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
         }
-        Arrays.sort(nums1);;        
     }
 }
