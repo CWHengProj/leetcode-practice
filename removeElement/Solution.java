bfef748eb3cf83f654fb8b76dd4d80a09cbdf2f2
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-06 14:00:24 +0800

    added recommended Solution
    Solution involves iterating through nums, ignoring the matches to val, and adding back to nums the valid numbers, while j is the number of values in nums at the end of it.

--- removeElement/Solution.java
+++ removeElement/Solution.java
@@ -1,20 +1,13 @@
 
 class Solution {
     public static int removeElement(int[] nums, int val) {
-        int k=0;
-        //run through all the values in nums, and remove any val that matches. increase the count of k. return k in the end.
-        int[] numArr = new int[nums.length];
-        int l =0;//position of the value in numArr the temp one
-        for (int i=0;i<nums.length;i++){
-            if (nums[i]==val){
-                k++;
-            }
-            else{
-                numArr[l]=nums[i];
-                l++;
+        int j=0;
+        for (int i=0; i<nums.length;i++){
+            if (val != nums[i]){
+                nums[j]=nums[i];
+                j++;
             }
         }
-        nums=numArr;
-        return k;
+        return j;
     }
-}
+}   
