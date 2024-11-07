cb59310ffa80f64480e8e7c74258916d7c778143
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-07 17:34:51 +0800

    To solve without converting to string

--- palindrome/Solution.java
+++ palindrome/Solution.java
@@ -2,8 +2,6 @@ class Solution {
     public boolean isPalindrome(int x) {
         String xString = String.valueOf(x);
         char [] xArr = xString.toCharArray();
-        //6/2 = 3, -1 =2
-        //7/2 = 3, -1 =2, 7%2 =1, 2+1 =3
         int centerPos = xArr.length/2 + xArr.length%2 -1;
         int j = centerPos; //add one if even number
         if (xArr.length%2 ==0){
