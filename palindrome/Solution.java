acbc35be7fcbaaa5f852243e3161576376ebc828
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-08 10:46:40 +0800

    Added solution without converting int X to string

--- palindrome/Solution.java
+++ palindrome/Solution.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String xString = String.valueOf(x);
-        char [] xArr = xString.toCharArray();
-        int centerPos = xArr.length/2 + xArr.length%2 -1;
-        int j = centerPos; //add one if even number
-        if (xArr.length%2 ==0){
-            j++;
+        //perform the same program without converting x into a string
+        //if x is negative, return false
+        //how to find center integer and then compare the front and back?
+        if (x<0){
+            return false;
         }
-        for (int i = centerPos; i>=0; i--){
-            if (xArr[i]!=xArr[j]){
-                return false;
-            }
-            j++;
+        int compare =0;
+        int tempX=x;
+        while (tempX>0){//while x is a double digit number, since single digit would mean is centrally symm
+            //10401 - remainder of 10 gives the last digit
+            compare = compare*10 + tempX%10;
+            tempX/=10;  
         }
-        return true;
-
+        if (x==compare){
+            return true;
+        }
+        return false;
     }
 }
