486c9413f9ded6f83b5fdbe81e640a0e7afea555
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-10 11:59:38 +0800

    Completed validParantheses v1

--- validParantheses/Solution.java
+++ validParantheses/Solution.java
@@ -4,42 +4,42 @@ class Solution {
     public boolean isValid(String s) {
        //check for valid parantheses - my approach would be using stack and pop to check if the closing
        //is the right pair as the opening, the most recent has to be closed by the same complement.
-       Stack<Character> STACK = new Stack<>(); 
+       Stack<Character> STACK = new Stack<>();
        //loop and add the characters to the stack, check the next one for a match
        for (char x: s.toCharArray()){
+        if (STACK.isEmpty()){
+            STACK.push(x);
+            continue;
+        }
         switch (x){
             case ')':
-                if (STACK.peek()=='('){
-                    STACK.pop();
-                }
-                else{
+                if (STACK.pop()!='('){
                     return false;
                 }
                 break;
             case ']':
-                if (STACK.peek()=='['){
-                    STACK.pop();
-                }
-                else{
+                if (STACK.pop()!='['){
                     return false;
                 }
                 break;
             case '}':
-                if (STACK.peek()=='{'){
-                    STACK.pop();
-                }
-                else{
+                if (STACK.pop()!='{'){
                     return false;
                 }
                 break;
             default:
                 STACK.push(x);    
         }
-        //push peek pop
+        //push peep pop
         //if current x is not a complement of the peep, return false
         //else, pop the most recent and don't add on
        }
-       return true;
+       if (STACK.isEmpty()){
+           return true;
+       }
+       else{
+        return false;
+       }
 
     }
 }
