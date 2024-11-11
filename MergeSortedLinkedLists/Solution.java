13f883e8166279600c36c907591d68b3d5557df4
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-11 10:19:53 +0800

    Added incomplete code for MergeSortedLinkedLists
    Not sure how to approach this data type, will look at solutions and try again after understanding

---
+++ MergeSortedLinkedLists/Solution.java
@@ -0,0 +1,40 @@
+import java.lang.classfile.components.ClassPrinter.ListNode;
+import java.util.LinkedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        //idealogy - to run through both lists, and compare the integers, slot in the numbers in order.
+        LinkedList<Integer> finalList = new LinkedList<>();
+        //compare sizes for both lists
+        int x = list1.size()>list2.size() ? list1.size(): list2.size();
+        int i,j =0;
+        while (i<list1.size() && j<list2.size()){
+            if (list1.get(i)<list2.get(j)){
+                finalList.add(list1.get[i]);
+                i++;
+            }
+            else if (list1.get(i)>list2.get(j)){
+                finalList.add(list1.get[j]);
+                j++;
+            }
+            else{
+                finalList.add(list1.get[i]);
+                finalList.add(list1.get[j]);
+                i++;
+                j++; 
+            }
+            
+        }
+        return finalList;
+    }
+}
