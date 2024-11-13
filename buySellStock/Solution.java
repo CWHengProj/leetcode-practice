0fc0171c03990e402596dd3d809cf8ba5a4442c6
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-13 09:57:28 +0800

    Added the model Solution
    Model soln goes through the array once, updating the lowest on the left if it is lower, and highest on the right if value is higher and greater than the max profit

--- buySellStock/Solution.java
+++ buySellStock/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
         //given an array of integers, return the highest profit possible
-        int maxProfit = -199999;
+        int maxProfit = 0;
+        int buyLow = prices[0]; //start from the first int, if there is a lower value means there is potential to make more profit
         //want to loop through the array, and exclude the rest of the integers
         for (int i =0 ; i<prices.length ; i++){
-            int j = i+1;
-            while (j<prices.length){
-                if ((prices[j]- prices[i])>maxProfit){
-                    maxProfit = prices[j]- prices[i];
-                }
-                j++;
+            if (prices[i]<buyLow){
+                buyLow = prices[i]; //ignores if there is no new lowest
+            }
+            else if (prices[i]-buyLow > maxProfit){
+                maxProfit = prices[i] - buyLow; //iterate through and find the best profit if there is higher val
             }
         }
-        return maxProfit>=0 ? maxProfit: 0;
+        return maxProfit;
     }
 }
