8f0ddb3c3db8c23acfa9848b181ba45b9d7998e5
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-13 17:40:26 +0800

    Added two pointer solution
    Can optimize the efficiency

--- isPalindrome/Solution.java
+++ isPalindrome/Solution.java
@@ -1,20 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
         //idea - to clean up the data , split from the center and reverse one of the substrings. Check if they are the same.
-        String allChars = "[^a-zA-Z]";
-        String cleanedString = s.replace(allChars, "");
+        String allChars = "[^a-zA-Z0-9]";
+        String cleanedString = s.replaceAll(allChars, "").toLowerCase();
         //find the length of the string
-        int strLength = cleanedString.length();
-        int halfLength = strLength/2 -1;
-        //invert the substring of the back half, and compare to the front half
-        char[] frontHalf = cleanedString.substring(0,halfLength).toCharArray();
-        char[] backHalf = cleanedString.substring((strLength/2+strLength%2),(strLength-1)).toCharArray();
-        int j =0;
-        for (int i=halfLength; i>0; i++){
-            if (backHalf[i]!=frontHalf[j]){
+        int left = 0;
+        int right = cleanedString.length()-1;
+        while (left < right){
+            if (cleanedString.charAt(left)!=cleanedString.charAt(right)){
                 return false;
             }
-            j++;
+            left++;
+            right--;
         }
         return true;
     }
