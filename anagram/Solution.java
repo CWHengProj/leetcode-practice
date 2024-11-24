9cc63a13957ca02bec325def5665e05bdb91e5a6
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-24 10:50:50 +0800

    Added initial solution for anagrams

--- anagram/Solution.java
+++ anagram/Solution.java
@@ -1,3 +1,21 @@
-public class Solution {
-    //initial idea - to iterate through S and T, to check if there are matches
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        //check if strings s and t are anagrams.
+        //anagrams means that they contain the same amount of characters with the same values
+        if (s.length()!=t.length()){
+            return false;
+        }
+        List<String> tList = new ArrayList<>(Arrays.asList(t.toLowerCase().split("")));
+        //assign t to an ArrayList, remove whenever there is a match
+        for (String sArray: s.toLowerCase().split("")){
+            if (!tList.remove(sArray)){
+                return false;
+            }
+        }
+        return true;
+    }
 }
