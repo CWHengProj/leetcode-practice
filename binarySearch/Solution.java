452e5cfe286c3f5c585cb7a097b22651c3d08e45
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-25 10:39:52 +0800

    completed binarysearch, weak at indexes and calculation of middle position

--- binarySearch/Solution.java
+++ binarySearch/Solution.java
@@ -1,5 +1,35 @@
+import java.util.Arrays;
+
 class Solution {
     public int search(int[] nums, int target) {
+        //search through nums, return the index of the target
+        //if target does not exist within nums, return -1
+        //nums is sorted in ascending order : we can use the value to compare to target.
+        //binary search works by checking if the indexed value is higher or lower than target,
+        //then eliminating/ignoring the other half.
         
+        //do non recursion first
+        //find current size of the array
+        int start = 0;
+        int end = nums.length -1;
+        //find the middle position
+        int currentGuess = nums.length/2;
+        //compare the value with target
+        //end condition - find the loop or start and end is the same
+        while(nums[currentGuess]!=target && (start!=end)){
+            if (nums[currentGuess]>target){
+                //calculate the next guess
+                end = currentGuess-1;
+            }
+            else{
+                start = currentGuess + 1;
+            }
+            currentGuess = start + (end - start)/2;
+        }
+            if (target==nums[currentGuess]){
+            //base case
+            return currentGuess;
+        }
+            return -1;
     }
 }
