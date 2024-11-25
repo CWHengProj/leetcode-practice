edea92406d59b014496c8411600d152f1b9447c4
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-25 09:57:16 +0800

    Started on binarysearch

--- anagram/Solution.java
+++ anagram/Solution.java
@@ -1,21 +1,13 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //check if strings s and t are anagrams.
-        //anagrams means that they contain the same amount of characters with the same values
-        if (s.length()!=t.length()){
-            return false;
-        }
-        List<String> tList = new ArrayList<>(Arrays.asList(t.toLowerCase().split("")));
-        //assign t to an ArrayList, remove whenever there is a match
-        for (String sArray: s.toLowerCase().split("")){
-            if (!tList.remove(sArray)){
-                return false;
-            }
-        }
-        return true;
+        //anagrams contain the same letters with the same size
+        //if we are able to sort the characters, we should be able to check if they are identical.
+        char[] sChar = s.toCharArray();
+        char[] tChar = t.toCharArray();
+        Arrays.sort(sChar);
+        Arrays.sort(tChar);
+        return Arrays.equals(sChar,tChar);
     }
 }
