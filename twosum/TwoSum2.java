0c5b176f7b165ed0b8ac61470ab864713561f432
CW <142023227+CWHengProj@users.noreply.github.com>
2025-01-03 10:22:04 +0800

    to prestige the old ones after going through some dsa basics

---
+++ twosum/TwoSum2.java
@@ -0,0 +1,8 @@
+public class TwoSum2 {
+    public int[] twoSum(int[] nums, int target) {
+    //issue with previous solution was the nesting of for loops - can i do it with just 1 for loop?
+    // idea: to only go through 1 loop.
+    // idea: search through by creating a map
+    
+    }
+}
